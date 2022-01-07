66b4741d060a98ed30a9ae92ddc86ffdeb798080
keyehzy <msamuel@aluno.puc-rio.br>
2022-01-07 18:41:13 -0300

    Tokenizer single character

--- regex.c
+++ regex.c
@@ -1,7 +1,47 @@
 #include "bdd.h"
 
-spec("simple") {
-  it("trivial example") {
-    check(1);
+typedef enum {
+  TOK_CHAR,
+} token_type;
+
+typedef struct {
+  token_type type;
+  const char* begin;
+  unsigned int size;
+} token;
+
+typedef struct {
+  const char *position;
+  const char *original_input;
+  token last_token;
+} tokenizer;
+
+token tokenizer_next(tokenizer *state) {
+  const char *begin = state->position;
+
+  token t = {0};
+
+  switch(begin[0]) {
+    default:
+      state->position += 1;
+      return (token) {.type = TOK_CHAR, .begin = begin, .size  = 1};
+  }
+
+  return t;
+}
+
+tokenizer new_tokenizer(const char* string) {
+  tokenizer t = {0};
+  t.position = string;
+  t.original_input = string;
+  t.last_token = tokenizer_next(&t);
+  return t;
+}
+
+spec("regex") {
+  it("should read a single character") {
+    tokenizer tokenizer = new_tokenizer("a");
+    token t = tokenizer.last_token;
+    check(t.type == TOK_CHAR);
   }
 }
